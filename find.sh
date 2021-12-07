ccc65531536f70a8d3a0f79915adeced38676da5
Sadi <shifat2sadi@gmail.com>
2021-12-07 20:46:24 +0200

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,5 +1,7 @@
 
 # Find files which match a specific pattern
+# https://stackoverflow.com/questions/16956810/how-do-i-find-all-files-containing-specific-text-on-linux
+
 grep -rnw '/path/to/somewhere/' -e 'pattern' 
 
-# https://stackoverflow.com/questions/16956810/how-do-i-find-all-files-containing-specific-text-on-linux
+
