0317bc44f924ef234da95da36f8d6546ec3280b5
Sadi <shifat2sadi@gmail.com>
2021-12-07 20:45:05 +0200

    Update find.sh

--- find.sh
+++ find.sh
@@ -1,3 +1,5 @@
 
 # Find files which match a specific pattern
-grep -rnw '/path/to/somewhere/' -e 'pattern'
+grep -rnw '/path/to/somewhere/' -e 'pattern' 
+
+# https://stackoverflow.com/questions/16956810/how-do-i-find-all-files-containing-specific-text-on-linux
