5dd93ce001cb33dc7b87c5b5e63c9401fc51795f
Sadi <shifat2sadi@gmail.com>
2021-12-09 20:43:19 +0200

    Update find.sh

--- find.sh
+++ find.sh
@@ -6,6 +6,12 @@ grep -rnw '/path/to/somewhere/' -e 'pattern'
 
 
 # find patterns and then sort alphabatecally 
-
 grep -rnw '/path/to/somewhere/' -e 'pattern' | cut -d '/' -f 2 | sort
+
+
+# Find sorted uniq lines
+grep -rnw '/path/to/somewhere/' -e 'pattern' | cut -d '/' -f 2 | sort | uniq
+
+# Find only duplicated items but repeated once
+grep -rnw '/path/to/somewhere/' -e 'pattern' | cut -d '/' -f 2 | sort | uniq -d
 
