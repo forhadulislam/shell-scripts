65060183b9d6fcb278ae50ae16d07bbba177bfe1
Sadi <shifat2sadi@gmail.com>
2021-12-10 12:00:28 +0200

    Update diff.sh

--- diff.sh
+++ diff.sh
@@ -1,5 +1,6 @@
 
-You can use diff on two strings, if you make files from them, because diff will only ever compare files
-https://unix.stackexchange.com/questions/582796/using-the-diff-command-to-compare-two-strings
+# You can use diff on two strings, if you make files from them, because diff will only ever compare files
+# https://unix.stackexchange.com/questions/582796/using-the-diff-command-to-compare-two-strings
 
-  diff <( printf '%s\n' "$string1" ) <( printf '%s\n' "$string2" )
+
+diff <( printf '%s\n' "$string1" ) <( printf '%s\n' "$string2" )
